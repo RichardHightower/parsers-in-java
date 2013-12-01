66adc4bccf83e165a595d6a8dc6c46a8e5918f5f
javaboon <richard.m.hightower@gmail.com>
2013-11-30 23:04:58 -0800

    winning two out of three.. and coming in second place in the one not winning in

--- src-benchmark/com/jenkov/parsers/round2/ObjectInputSerialization.java
+++ src-benchmark/com/jenkov/parsers/round2/ObjectInputSerialization.java
@@ -27,6 +27,30 @@ public class ObjectInputSerialization {
         List<BenchMark> benchMarks =  buildBenchMarkList ( fileContents, 100_000 );
 
 
+
+        puts ("Warming up the monkeys!");
+
+
+
+
+        for (BenchMark benchMark : benchMarks) {
+            benchMark.test ();
+        }
+
+        processRun ( file, buildBenchMarkListFullObject ( fileContents, 1_000_000 ),  "Full object" );
+
+
+        processRun ( file, buildBenchMarkListNoSubListNoSubItem ( fileContents, 1_000_000 ),  "No sub item, no sum list" );
+
+
+        processRun ( file, buildBenchMarkListNoSublist ( fileContents, 1_000_000 ),  "No sub list" );
+
+    }
+
+    private static void processRun( String file, List<BenchMark> benchMarks, String name ) {
+
+        puts("_______________________________", name ,"____________________________________________");
+
         puts ( Str.rpad ( "Name", 20, ' ' ),  Str.rpad("Time", 20, ' '),
                 Str.lpad ("File",  20, ' '),
                 Str.lpad("Iterations", 20, ' '));
@@ -37,31 +61,22 @@ public class ObjectInputSerialization {
 
         for (BenchMark benchMark : benchMarks) {
             benchMark.test ();
-        }
-
-
-        benchMarks =  buildBenchMarkList ( fileContents, 200_000 );
-
-        for (BenchMark benchMark : benchMarks) {
-            benchMark.test ();
             if (benchMark.finalTime < min) {
 
                 min = benchMark.finalTime;
                 winner = benchMark.name;
             }
-            puts (Str.rpad ( benchMark.name, 20, ' '),
+            puts ( Str.rpad ( benchMark.name, 20, ' ' ),
                     Str.rpad(String.format("%,d", benchMark.finalTime), 20, ' '),
-                    Str.lpad (""+ IO.path (file).getFileName (),  20, ' '),
+                    Str.lpad (""+ IO.path ( file ).getFileName (),  20, ' '),
                     Str.lpad( String.format ("%,d", benchMark.times), 20, ' '));
         }
 
         puts ("Winner:", winner);
 
         puts("___________________________________________________________________________________");
+
     }
-
-
-
 
 
     private static List<BenchMark> buildBenchMarkList(final String fileContents, int times) {
@@ -197,6 +212,158 @@ public class ObjectInputSerialization {
                     void run() {
 
                         JsonLazyEncodeParser.fullParseInto ( AllTypes3.class, chars );
+
+                    }
+                }
+
+        );
+    }
+
+
+
+
+    private static List<BenchMark> buildBenchMarkListNoSublist(final String fileContents, int times) {
+        return Lists.list (
+                new BenchMark ( "jackson al2 ", times, fileContents ) {
+                    ObjectMapper mapper = new ObjectMapper ();
+
+
+                    {
+                        mapper.configure ( DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false );
+                    }
+
+
+                    @Override
+                    void run() {
+                        try {
+                            mapper.readValue ( fileContents, AllTypes2.class );
+                        } catch ( IOException e ) {
+                            e.printStackTrace ();
+                        }
+                    }
+                },
+                new BenchMark ( "gson  al2 ", times, fileContents ) {
+                    Gson gson = new Gson ();
+
+                    @Override
+                    void run() {
+                        gson.fromJson ( fileContents, AllTypes2.class );
+
+                    }
+                },
+                new BenchMark ( "boon lazy c 2", times, fileContents ) {
+
+                    @Override
+                    void run() {
+
+                        JsonLazyEncodeParser.parseInto ( AllTypes2.class, chars );
+
+                    }
+                },
+                new BenchMark ( "boon full c 2", times, fileContents ) {
+
+                    @Override
+                    void run() {
+
+                        JsonLazyEncodeParser.fullParseInto ( AllTypes2.class, chars );
+
+                    }
+                }
+        );
+    }
+
+    private static List<BenchMark> buildBenchMarkListNoSubListNoSubItem(final String fileContents, int times) {
+        return Lists.list (
+                new BenchMark ( "jackson al3 ", times, fileContents ) {
+                    ObjectMapper mapper = new ObjectMapper ();
+
+
+                    {
+                        mapper.configure ( DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false );
+                    }
+
+
+                    @Override
+                    void run() {
+                        try {
+                            mapper.readValue ( fileContents, AllTypes3.class );
+                        } catch ( IOException e ) {
+                            e.printStackTrace ();
+                        }
+                    }
+                },
+                new BenchMark ( "gson  al3 ", times, fileContents ) {
+                    Gson gson = new Gson ();
+
+                    @Override
+                    void run() {
+                        gson.fromJson ( fileContents, AllTypes3.class );
+
+                    }
+                },
+                new BenchMark ( "boon lazy c3", times, fileContents ) {
+
+                    @Override
+                    void run() {
+
+                        JsonLazyEncodeParser.parseInto ( AllTypes3.class, chars );
+
+                    }
+                },
+                new BenchMark ( "boon full c3", times, fileContents ) {
+
+                    @Override
+                    void run() {
+
+                        JsonLazyEncodeParser.fullParseInto ( AllTypes3.class, chars );
+
+                    }
+                }
+        );
+    }
+
+    private static List<BenchMark> buildBenchMarkListFullObject(final String fileContents, int times) {
+        return Lists.list (
+                new BenchMark ( "jackson", times, fileContents ) {
+                    ObjectMapper mapper = new ObjectMapper ();
+
+                    {
+                        mapper.configure ( DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false );
+                    }
+
+                    @Override
+                    void run() {
+                        try {
+                            mapper.readValue ( fileContents, AllTypes.class );
+                        } catch ( IOException e ) {
+                            e.printStackTrace ();
+                        }
+                    }
+                },
+                new BenchMark ( "gson   ", times, fileContents ) {
+                    Gson gson = new Gson ();
+
+                    @Override
+                    void run() {
+                        gson.fromJson ( fileContents, AllTypes.class );
+
+                    }
+                },
+                new BenchMark ( "boon lazy c 1", times, fileContents ) {
+
+                    @Override
+                    void run() {
+
+                        JsonLazyEncodeParser.parseInto ( AllTypes.class, chars );
+
+                    }
+                },
+                new BenchMark ( "boon full c 1", times, fileContents ) {
+
+                    @Override
+                    void run() {
+
+                        JsonLazyEncodeParser.fullParseInto ( AllTypes.class, chars );
 
                     }
                 }
